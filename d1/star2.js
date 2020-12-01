50e6884e84480aaeec4b4a0f6e0933f7ce673229
Gio <personal@gio.dev>
2020-12-01 17:45:21 +0100

    Day 01

---
+++ d1/star2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+fs.readFile(__dirname + '/input.txt', (_, data) => {
+  const numbers = data
+    .toString()
+    .split('\n')
+    .sort((a, b) => b - a);
+  const goal = 2020;
+  let found = 0,
+    found2 = 0,
+    next = 0;
+  while (!found && numbers.length) {
+    next = +numbers.pop();
+    if (!next) continue;
+    found = numbers.find((number) => {
+      const newGoal = goal - next;
+      if (!number || +number > newGoal) return false;
+      found2 = numbers.find((number2) => +number + +number2 === newGoal);
+      return !!found2;
+    });
+  }
+  console.log(next * found * found2);
+});
