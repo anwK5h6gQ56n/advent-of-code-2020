3b0fe4403ca22f1904d26fbb20ee8650edc954c2
Gio <MvOc0yRHIof9wo8OF@gio.dev>
2020-12-04 13:19:46 +0100

    Day 04

--- 04/index.ts
+++ 04/index.ts
@@ -36,20 +36,13 @@ class Passport {
 	}
 
 	isValid(): boolean {
-		const isFourDigits = /^\d{4}$/;
 		const hgt = this.hgt.split(/(in|cm)/);
 		return !!(
-			this.byr.match(isFourDigits) &&
-			+this.byr >= 1920 &&
-			+this.byr <= 2002 &&
-			this.iyr.match(isFourDigits) &&
-			+this.iyr >= 2010 &&
-			+this.iyr <= 2020 &&
-			this.eyr.match(isFourDigits) &&
-			+this.eyr >= 2020 &&
-			+this.eyr <= 2030 &&
+			this.byr.match(/^(19[2-9][0-9]|200[012])$/) &&
+			this.iyr.match(/^(201[0-9]|2020)$/) &&
+			this.eyr.match(/^(202[0-9]|2030)$/) &&
 			this.hcl.match(/^#([a-f0-9]{6})$/) &&
-			['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(this.ecl) &&
+			this.ecl.match(/^amb|blu|brn|gry|grn|hzl|oth$/) &&
 			this.pid.match(/^\d{9}$/) &&
 			this.hgt.match(/^\d{2,3}(in|cm)$/) &&
 			((hgt[1] === 'in' && +hgt[0] >= 59 && +hgt[0] <= 76) || (hgt[1] === 'cm' && +hgt[0] >= 150 && +hgt[0] <= 193))
