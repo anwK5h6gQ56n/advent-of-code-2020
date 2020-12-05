47d544f1b1c48c12b6a5b116d3d9bd3a8bd8bb89
Gio <personal@gio.dev>
2020-12-05 06:56:01 +0100

    Day 05

--- 05/index.ts
+++ 05/index.ts
@@ -1,42 +1,22 @@
 export function part1(input: string) {
-	return processSeats(input).sort((a, b) => b.id - a.id)[0].id;
+	return processSeats(input).sort((a, b) => b - a)[0];
 }
 
 export function part2(input: string) {
-	const seats = processSeats(input).map((x) => x.id);
+	const seats = processSeats(input);
 	return Array.from({ length: Math.max(...seats) }, (_, x) => (!seats.includes(x + 1) ? x + 1 : false))
 		.filter(Boolean)
 		.reverse()[0];
 }
 
-function processSeats(data: string): { row: number; col: number; id: number }[] {
+function processSeats(data: string): number[] {
 	return data.split('\n').map((seatBinary: string) => {
 		const rowNo = [...Array(128).keys()];
 		const colNo = [...Array(8).keys()];
 		seatBinary.split('').forEach((x: string) => {
-			switch (x) {
-				case 'F': {
-					rowNo.splice(rowNo.length / 2, rowNo.length / 2);
-					break;
-				}
-				case 'B': {
-					rowNo.splice(0, rowNo.length / 2);
-					break;
-				}
-				case 'L': {
-					colNo.splice(colNo.length / 2, colNo.length / 2);
-					break;
-				}
-				case 'R': {
-					colNo.splice(0, colNo.length / 2);
-					break;
-				}
-			}
+			const array = x.match(/[FB]/) ? rowNo : colNo;
+			array.splice(x.match(/[FL]/) ? array.length / 2 : 0, array.length / 2);
 		});
-		return {
-			id: rowNo[0] * 8 + colNo[0],
-			row: rowNo[0],
-			col: colNo[0],
-		};
+		return rowNo[0] * 8 + colNo[0];
 	});
 }
