23ec7d983fedb0ed9630531d2712b3a1c23ac6c6
Gio <personal@gio.dev>
2020-12-06 13:56:04 +0100

    Day 06

---
+++ 06/index.ts
@@ -0,0 +1,21 @@
+export function part1(input: string): number {
+	return processGroups(input)
+		.map((peopleAnswers: string[][]) => peopleAnswers.reduce((a, b) => [...a, ...b]))
+		.map((answers: string[]) => answers.filter((x, i) => answers.indexOf(x) === i).length)
+		.reduce((a, b) => a + b);
+}
+
+export function part2(input: string): number {
+	return processGroups(input)
+		.map(
+			(peopleAnswers) =>
+				peopleAnswers[0].filter((firstPersonAnswer) =>
+					peopleAnswers.every((personAnswers) => personAnswers.includes(firstPersonAnswer)),
+				).length,
+		)
+		.reduce((a, b) => a + b);
+}
+
+function processGroups(input: string): string[][][] {
+	return input.split('\n\n').map((block) => block.split('\n').map((person) => person.split('')));
+}
