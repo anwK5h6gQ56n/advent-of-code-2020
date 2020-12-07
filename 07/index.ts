c411789cb392652d380fc6f32e2d27b6d2cba725
Gio <personal@gio.dev>
2020-12-07 19:32:55 +0100

    Day 07

--- 07/index.ts
+++ 07/index.ts
@@ -14,9 +14,9 @@ interface Rules {
 }
 
 function processRules(data: string): Rules {
-	return [...data.matchAll(/((^\w+\b.*) bags contain (.*)\.)/gm)].reduce((a, b) => {
-		(a as any)[b[2]] = b[3].split(', ').reduce((c, d) => {
-			const types = [...d.matchAll(/((\d+) (.*) bags?)/g)][0];
+	return [...data.matchAll(/((^\w+\b.*) bags contain (.*)\.)/gm)].reduce((a: Rules, b: Array<string>) => {
+		(a as any)[b[2]] = b[3].split(', ').reduce((c: { [key: string]: number }, d: string) => {
+			const types: Array<string> = [...d.matchAll(/((\d+) (.*) bags?)/g)][0];
 			if (types && types[3]) (c as any)[types[3]] = +types[2];
 			return c;
 		}, {});
@@ -31,9 +31,8 @@ function containsBag(rules: Rules, colour: string, target: string): boolean {
 
 function countBags(rules: Rules, colour: string): number {
 	const rule = rules[colour];
-	return Object.keys(rule).reduce((a: number, b: string) => {
-		if (!rule[b]) return a;
-		console.log(`${rule[b]} * ${b}`);
-		return a + rule[b] + rule[b] * countBags(rules, b);
-	}, 0);
+	return Object.keys(rule).reduce(
+		(a: number, b: string) => (rule[b] ? a + rule[b] + rule[b] * countBags(rules, b) : a),
+		0,
+	);
 }
