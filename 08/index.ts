66e51b5371fa0c55bcc7c4d5b0bd6aaa764ed062
Gio <personal@gio.dev>
2020-12-08 10:40:05 +0100

    Day 08

--- 08/index.ts
+++ 08/index.ts
@@ -1,60 +1,26 @@
 export function part1(input: string): number {
-	const instructions = processInstructions(input);
-	let accumulator: number = 0;
-	let index: number = 0;
-	let previous: Instruction[] = [];
-
-	while (true) {
-		const current: Instruction = instructions[index];
-		if (previous.includes(current)) return accumulator;
-		previous.push(current);
-		switch (current.operation) {
-			case Operation.Accumulate:
-				accumulator += current.value;
-			case Operation.NoOperation:
-				index++;
-				break;
-			case Operation.Jump:
-				index += current.value;
-				break;
-		}
-	}
+	return execute(processInstructions(input)).result;
 }
 
-export function part2(input: string) {
+export function part2(input: string): number | undefined {
 	const instructions = processInstructions(input);
 	for (const instruction of instructions) {
 		if (instruction.operation === Operation.Accumulate) continue;
-		let accumulator: number = 0;
-		let index: number = 0;
-		let previous: Instruction[] = [];
-		while (true) {
-			const current: Instruction = instructions[index];
-			if (!current || previous.includes(current)) break;
-			previous.push(current);
-			let operation = current.operation;
-			if (instruction === current) {
-				switch (current.operation) {
-					case Operation.NoOperation:
-						operation = Operation.Jump;
-						break;
-					case Operation.Jump:
-						operation = Operation.NoOperation;
-						break;
+		const run = execute(
+			instructions,
+			(current: Instruction): Operation => {
+				if (instruction === current) {
+					switch (current.operation) {
+						case Operation.NoOperation:
+							return Operation.Jump;
+						case Operation.Jump:
+							return Operation.NoOperation;
+					}
 				}
-			}
-			switch (operation) {
-				case Operation.Accumulate:
-					accumulator += current.value;
-				case Operation.NoOperation:
-					index++;
-					break;
-				case Operation.Jump:
-					index += current.value;
-					break;
-			}
-			if (index === instructions.length) return accumulator;
-		}
+				return current.operation;
+			},
+		);
+		if (run.completed) return run.result;
 	}
 }
 
@@ -77,4 +43,32 @@ function processInstructions(data: string): Instruction[] {
 			value: +match[2],
 		};
 	});
+}
+
+function execute(
+	instructions: Instruction[],
+	changeOperation?: (instruction: Instruction) => Operation,
+): { result: number; completed: boolean } {
+	let accumulator: number = 0;
+	let index: number = 0;
+	let previous: Instruction[] = [];
+
+	while (true) {
+		const current: Instruction = instructions[index];
+		if (previous.includes(current)) break;
+		previous.push(current);
+		let operation = changeOperation ? changeOperation(current) : current.operation;
+		switch (operation) {
+			case Operation.Accumulate:
+				accumulator += current.value;
+			case Operation.NoOperation:
+				index++;
+				break;
+			case Operation.Jump:
+				index += current.value;
+				break;
+		}
+		if (index === instructions.length) return { result: accumulator, completed: true };
+	}
+	return { result: accumulator, completed: false };
 }
