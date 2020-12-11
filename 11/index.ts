21b5da9dcb126279448808d79fcedcd7669e7a99
Gio <personal@gio.dev>
2020-12-11 13:47:51 +0100

    Day 11

--- 11/index.ts
+++ 11/index.ts
@@ -1,24 +1,28 @@
 // prettier-ignore
 const NEIGHBOR_DIR = [[-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]];
 
-export const part1 = (input: string) => getFilledSeats(input, false);
+export const part1 = (input: string): number => getFilledSeats(input, false);
 
-export const part2 = (input: string) => getFilledSeats(input, true);
+export const part2 = (input: string): number => getFilledSeats(input, true);
 
-function getFilledSeats(input: string, los: boolean) {
+function getFilledSeats(input: string, los: boolean): number {
 	let seatGrid = new SeatGrid(input);
 	let nextGrid: SeatGrid | null = null;
 	while (nextGrid === null || seatGrid.hasChangedFrom(nextGrid)) {
 		if (nextGrid !== null) seatGrid = nextGrid;
 		nextGrid = seatGrid.process((cell, x, y) => {
 			const neighbors = los ? seatGrid.getSeatLosNeighbors(x, y) : seatGrid.getSeatNeighbors(x, y);
-			if (cell?.value === 'L' && neighbors.every((neighbor) => neighbor?.value !== '#')) return '#';
-			if (cell?.value === '#' && neighbors.filter((neighbor) => neighbor?.value === '#').length > (los ? 4 : 3))
-				return 'L';
+			if (cell?.value === CellValue.Empty && neighbors.every((neighbor) => neighbor?.value !== CellValue.Occupied))
+				return CellValue.Occupied;
+			if (
+				cell?.value === CellValue.Occupied &&
+				neighbors.filter((neighbor) => neighbor?.value === CellValue.Occupied).length > (los ? 4 : 3)
+			)
+				return CellValue.Empty;
 			return cell.value;
 		});
 	}
-	return seatGrid.cells.flat().filter((x) => x.value === '#').length;
+	return seatGrid.cells.flat().filter((x) => x.value === CellValue.Occupied).length;
 }
 
 class SeatGrid {
@@ -31,14 +35,14 @@ class SeatGrid {
 		NEIGHBOR_DIR.map(([Δx, Δy]) => {
 			for (let index = 1; true; index++) {
 				let cell = this.getSeat(x + Δx * index, y + Δy * index);
-				if (cell?.value !== '.') return cell;
+				if (cell?.value !== CellValue.Floor) return cell;
 			}
 		});
-	setSeat = (x: number, y: number, value: '#' | 'L' | '.' | ''): void => {
+	setSeat = (x: number, y: number, value: CellValue): void => {
 		const seat = this.getSeat(x, y);
 		if (seat) seat.value = value;
 	};
-	process = (x: (cell: SeatCell, x: number, y: number) => '#' | 'L' | '.' | ''): SeatGrid => {
+	process = (x: (cell: SeatCell, x: number, y: number) => CellValue): SeatGrid => {
 		const seatGrid: SeatGrid = new SeatGrid(this.print());
 		this.cells.forEach((row, yIndex) =>
 			row.forEach((cell, xIndex) => seatGrid.setSeat(xIndex, yIndex, x(cell, xIndex, yIndex))),
@@ -47,12 +51,17 @@ class SeatGrid {
 	};
 
 	constructor(data: string) {
-		this.cells = data
-			.split('\n')
-			.map((y) => y.split('').map((x) => new SeatCell(x as '#' | 'L' | '.'))) as SeatCell[][];
+		this.cells = data.split('\n').map((y) => y.split('').map((x) => new SeatCell(x as CellValue))) as SeatCell[][];
 	}
 }
 
 class SeatCell {
-	constructor(public value: '#' | 'L' | '.' | '') {}
+	constructor(public value: CellValue) {}
+}
+
+enum CellValue {
+	None = '',
+	Empty = 'L',
+	Occupied = '#',
+	Floor = '.',
 }
