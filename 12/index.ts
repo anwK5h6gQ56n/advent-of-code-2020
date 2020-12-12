b1ec06344f8cc09dd72ee2d23098f13f99c445cb
Gio <personal@gio.dev>
2020-12-12 15:54:49 +0100

    Day 12

--- 12/index.ts
+++ 12/index.ts
@@ -1,44 +1,45 @@
+const dirActions = (loc: { x: number; y: number }): { [key: string]: (unit: number) => void } => ({
+	[Direction.North]: (unit) => (loc.y += unit),
+	[Direction.South]: (unit) => (loc.y -= unit),
+	[Direction.East]: (unit) => (loc.x += unit),
+	[Direction.West]: (unit) => (loc.x -= unit),
+});
+
 export function part1(input: string) {
 	// prettier-ignore
-	let x = 0, y = 0, dir = Direction.East;
+	let loc = {x: 0, y: 0}, dir = Direction.East;
 	const changeDirection = (unit: number, clockwise: boolean): Direction => {
 		let rotation = [Direction.North, Direction.East, Direction.South, Direction.West];
 		if (!clockwise) rotation = rotation.reverse();
 		return rotation[(rotation.indexOf(dir) + unit / 90) % rotation.length];
 	};
 	const actions: { [key: string]: (unit: number) => void } = {
-		[Direction.North]: (unit) => (y += unit),
-		[Direction.South]: (unit) => (y -= unit),
-		[Direction.East]: (unit) => (x += unit),
-		[Direction.West]: (unit) => (x -= unit),
+		...dirActions(loc),
 		[Direction.Left]: (unit) => (dir = changeDirection(unit, false)),
 		[Direction.Right]: (unit) => (dir = changeDirection(unit, true)),
 		[Direction.Forward]: (unit) => actions[dir](unit),
 	};
 	runInstructions(input, actions);
-	return Math.abs(x) + Math.abs(y);
+	return Math.abs(loc.x) + Math.abs(loc.y);
 }
 
 export function part2(input: string) {
 	// prettier-ignore
-	let x = 0, y = 0, waypointX = 10, waypointY = 1;
+	let loc = { x: 0, y: 0 },	waypointLoc = { x: 10, y: 1 };
 	runInstructions(input, {
-		[Direction.North]: (unit) => (waypointY += unit),
-		[Direction.South]: (unit) => (waypointY -= unit),
-		[Direction.East]: (unit) => (waypointX += unit),
-		[Direction.West]: (unit) => (waypointX -= unit),
+		...dirActions(waypointLoc),
 		[Direction.Left]: (unit) => {
-			for (let i = 1; i <= unit / 90; i++) [waypointX, waypointY] = [-waypointY, waypointX];
+			for (let i = 1; i <= unit / 90; i++) [waypointLoc.x, waypointLoc.y] = [-waypointLoc.y, waypointLoc.x];
 		},
 		[Direction.Right]: (unit) => {
-			for (let i = 1; i <= unit / 90; i++) [waypointX, waypointY] = [waypointY, -waypointX];
+			for (let i = 1; i <= unit / 90; i++) [waypointLoc.x, waypointLoc.y] = [waypointLoc.y, -waypointLoc.x];
 		},
 		[Direction.Forward]: (unit) => {
-			x += unit * waypointX;
-			y += unit * waypointY;
+			loc.x += unit * waypointLoc.x;
+			loc.y += unit * waypointLoc.y;
 		},
 	});
-	return Math.abs(x) + Math.abs(y);
+	return Math.abs(loc.x) + Math.abs(loc.y);
 }
 
 function runInstructions(data: string, actions: { [key: string]: (unit: number) => void }): void {
