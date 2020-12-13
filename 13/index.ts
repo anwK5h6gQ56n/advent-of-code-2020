1df40edb9fed5c9097ff5441f0fa22246a4e909f
Gio <personal@gio.dev>
2020-12-13 17:34:54 +0100

    Day 13

--- 13/index.ts
+++ 13/index.ts
@@ -16,15 +16,10 @@ export function part2(input: string) {
 	const [, buses] = input.split('\n');
 	const nixes = buses
 		.split(',')
-		.map((x: string, index: number): NIx => ({ num: +x, i: index }))
-		.filter((nindex: NIx) => !isNaN(nindex.num));
-	const n = nixes.reduce((a, b) => a * b.num, 1);
+		.map((x: string, i: number) => ({ n: +x, i }))
+		.filter((nindex) => !isNaN(nindex.n));
+	const n = nixes.reduce((a, b) => a * b.n, 1);
 	const mmi = (a: number, b: number): number => (!a || b % a == 0 ? 1 : b - Math.floor((mmi(b % a, a) * b) / a));
-	const x = nixes.reduce((a, b) => (a += b.i * Math.floor(n / b.num) * mmi(Math.floor(n / b.num), b.num)), 0);
+	const x = nixes.reduce((a, b) => (a += b.i * Math.floor(n / b.n) * mmi(Math.floor(n / b.n), b.n)), 0);
 	return n - (x % n) + nixes.length;
-}
-
-interface NIx {
-	num: number;
-	i: number;
 }
