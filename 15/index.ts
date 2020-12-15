b3e3eecc90d9759103499001cac613c144a1e59c
Gio <personal@gio.dev>
2020-12-15 11:15:41 +0100

    Day 15

--- 15/index.ts
+++ 15/index.ts
@@ -3,13 +3,10 @@ export const part1 = (input: string) => processNumbers(input, 2020);
 export const part2 = (input: string) => processNumbers(input, 30000000);
 
 function processNumbers(input: string, goal: number) {
-	const memory: { [key: string]: number } = input
-		.split(',')
-		.map((x) => +x)
-		.reduce((a, b, i) => {
-			(a as any)[`x${b}`] = i + 1;
-			return a;
-		}, {});
+	const memory = [...input.split(',')].reduce(
+		(a: { [key: string]: number }, b: string, i: number) => ((a[`x${b}`] = i + 1) as any) && a,
+		{},
+	);
 	let curr = +(Object.keys(memory).pop() || 0);
 	for (let turn = Object.keys(memory).length; turn < goal; turn++) {
 		const next = memory[`x${curr}`] ? turn - memory[`x${curr}`] : 0;
