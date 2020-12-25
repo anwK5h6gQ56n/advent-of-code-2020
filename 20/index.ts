b4950df5f93a95708f8f22f1d850b3df1ea87dcd
Gio <MvOc0yRHIof9wo8OF@gio.dev>
2020-12-25 17:19:02 +0100

    Day 25

--- 20/index.ts
+++ 20/index.ts
@@ -1,58 +1,22 @@
 export function part1(input: string): number {
-	return findEdgeTileIds(findTilesNeighbors(Tile.processTiles(input)))
-		.filter((tileId, i, tileIds) => tileIds.indexOf(tileId) !== i)
-		.reduce((a, b) => a * b);
+	const edges = new TileImage(input).edges;
+	return edges.reduce((a, b) => a * b);
 }
 
 export function part2(input: string) {
-	const image = new Image(input);
-	//return image.matrix;
-}
-
-function findTilesNeighbors(tiles: Tile[]): TileBorderNeighbors {
-	return tiles.reduce((borders, tile) => {
-		tile.borders.forEach((x) => {
-			// A border flipped is the same border.
-			const border = [x.join(''), x.reverse().join('')].sort()[0];
-			borders[border] ||= [];
-			borders[border].push(tile.id);
-		});
-		return borders;
-	}, {} as TileBorderNeighbors);
-}
-
-function findEdgeTileIds(tileBorderNeighbors: TileBorderNeighbors): number[] {
-	return Object.values(tileBorderNeighbors)
-		.filter((tileIds) => tileIds.length === 1)
-		.flat();
+	const image = new TileImage(input);
+	return image.countMonsters();
 }
 
 class Tile {
-	id: number;
-	coordinate: Coordinate = { x: 0, y: 0 };
-	rotated: number = 0;
-	flippedH: boolean = false;
-	flippedV: boolean = false;
-	processed: boolean = false;
+	readonly id: number;
+	readonly source: string[][];
+	neighborIds?: number[];
+	gridPosition?: { x: number; y: number; source: string[][] };
 
-	// Like CSS borders: top, right, bottom, left
-	get borders(): string[][] {
-		return [
-			this._borders[this.rotated % 4],
-			this._borders[(this.rotated + 1) % 4],
-			this._borders[(this.rotated + 2) % 4],
-			this._borders[(this.rotated + 3) % 4],
-		];
+	get borders(): string[] {
+		return Tile.getBordersFromSource(this.source);
 	}
-	private get _borders(): string[][] {
-		return [
-			this.source[0],
-			this.source.map((x) => x[x.length - 1]),
-			this.source[this.source.length - 1],
-			this.source.map((x) => x[0]),
-		];
-	}
-	private source: string[][];
 
 	constructor(input: string) {
 		const [, id, source] = input.match(/^Tile (\d+):\n([.#\n]+)/) || [];
@@ -60,100 +24,128 @@ class Tile {
 		this.source = source.split('\n').map((line) => line.split(''));
 	}
 
-	static processTiles = (input: string): Tile[] => input.split('\n\n').map((tileLns) => new Tile(tileLns));
+	static fromInput = (...input: string[]): Tile[] => input.map((tileLns) => new Tile(tileLns));
+
+	static getBordersFromSource(source: string[][]) {
+		return [
+			source[0].join(),
+			source[source.length - 1].join(),
+			source.map((x) => x[0]).join(),
+			source.map((x) => x[x.length - 1]).join(),
+		];
+	}
 }
 
-class Image {
-	matrix: { [key: number]: { [key: number]: Tile } };
-	private get matrixValues() {
-		return Object.values(this.matrix).flatMap((y) => Object.values(y).flatMap((x) => x));
+class TileImage {
+	tiles: Tile[];
+
+	get edges(): number[] {
+		return this.tiles.filter((tile) => (tile.neighborIds || []).length === 2).map((tile) => tile.id);
 	}
-	private tiles: Tile[];
-	private neighbors: TileBorderNeighbors;
 
 	constructor(input: string) {
-		this.tiles = Tile.processTiles(input);
-		this.neighbors = findTilesNeighbors(this.tiles);
-		const rootTile = findEdgeTileIds(this.neighbors)
-			.filter((tileId, i, tileIds) => tileIds.indexOf(tileId) !== i)
-			.map((x) => this.tiles.find((tile) => tile.id === x) || new Tile(''))[1];
-		this.matrix = { 0: { 0: rootTile } };
-		const findPosition = {
-			[Direction.Top]: (origin: Tile) => {
-				return { x: origin.coordinate.x, y: origin.coordinate.y - 1 };
-			},
-			[Direction.Right]: (origin: Tile) => {
-				return { x: origin.coordinate.x + 1, y: origin.coordinate.y };
-			},
-			[Direction.Bottom]: (origin: Tile) => {
-				return { x: origin.coordinate.x, y: origin.coordinate.y + 1 };
-			},
-			[Direction.Left]: (origin: Tile) => {
-				return { x: origin.coordinate.x - 1, y: origin.coordinate.y };
-			},
-		};
-		while (this.matrixValues.some((x) => !x.processed)) {
-			const notProcessed = [...this.matrixValues.filter((tile) => !tile.processed)];
-			console.log(`\n\n==NEW RUN`);
-			notProcessed.forEach((unprocessedTile) => {
-				this.findTilesForTile(unprocessedTile).forEach((relative) => {
-					relative.tile.coordinate = findPosition[relative.dir](unprocessedTile);
-					if (!(this.matrix[relative.tile.coordinate.y] ||= {})[relative.tile.coordinate.x])
-						(this.matrix[relative.tile.coordinate.y] ||= {})[relative.tile.coordinate.x] = relative.tile;
-				});
-				unprocessedTile.processed = true;
-			});
-		}
-		console.log(this.matrix);
+		this.tiles = Tile.fromInput(...input.split('\n\n'));
+		const getTileBorders = (tile: Tile) => [tile.borders, tile.borders.map((border) => [...border].reverse().join(''))];
+		this.tiles.forEach((tile: Tile, tileIndex: number) => {
+			tile.neighborIds = this.tiles
+				.filter((potentialNeighbor: Tile, index: number) => {
+					if (tileIndex === index) return false;
+					for (const tileBorders of getTileBorders(tile))
+						for (const potentialNeighborBorders of getTileBorders(potentialNeighbor))
+							if (new Set([...tileBorders, ...potentialNeighborBorders]).size < 8) return true;
+					return false;
+				})
+				.map((neighbor) => neighbor.id);
+		});
+		const index = this.tiles.findIndex((x) => x.id === this.edges[0]);
+		const tile = this.tiles[index];
+		this.tiles[index].gridPosition = { source: tile.source, x: 0, y: 0 };
+		this.positionNeighbors(index);
 	}
 
-	private findTilesForTile(tile: Tile): { dir: Direction; tile: Tile }[] {
-		let found: { dir: Direction; tile: Tile }[] = [];
-		for (let dir = Direction.Top; dir < 4; dir++) {
-			const border = tile.borders[dir].join('');
-			const neighborsForBorder = this.neighbors[border];
-			if (neighborsForBorder) {
-				const neighborTileId = neighborsForBorder.find((x) => tile.id !== x);
-				if (neighborTileId) {
-					const neighborTile = this.tiles.find((tile) => tile.id === neighborTileId);
-					if (neighborTile && !neighborTile.processed) {
-						for (let rotation = 0; rotation < 4; rotation++) {
-							neighborTile.rotated = rotation;
-							const foundTileOppositeBorder = neighborTile.borders[(dir + 2) % 4];
-							if (
-								foundTileOppositeBorder.join('') === border ||
-								foundTileOppositeBorder.reverse().join('') === border
-							) {
-								const flipped = foundTileOppositeBorder.join('') !== border;
-								if (dir === Direction.Top || dir === Direction.Bottom) {
-									neighborTile.flippedH = flipped;
-								} else {
-									neighborTile.flippedV = flipped;
-								}
-								break;
-							}
-						}
-						found.push({ dir, tile: neighborTile });
-					}
-				}
+	countMonsters() {
+		const monster = `                  #
+	  #    ##    ##    ###
+	   #  #  #  #  #  #   `;
+		const regex = monster.split('\n').map((x) => new RegExp(`(?=(${x.replace(/\s/g, '.')}))`, 'g'));
+		let count = 0;
+		for (const source of TileImage.possibleOrientationsGenerator(this.render())) {
+			const rows = source.map((chars) => chars.join(''));
+			for (let y = 0; y < rows.length - 2; y++) {
+				const x = (xa: (number | undefined)[], xb: (number | undefined)[]) => xa.filter((xc) => new Set(xb).has(xc));
+				const handleRow = (i: number): (number | undefined)[] =>
+					[...rows[y + i].matchAll(regex[i])].map((x) => x.index);
+				count += x(x(handleRow(0), handleRow(1)), handleRow(2)).length;
 			}
 		}
-		return found;
+		return count;
 	}
-}
 
-interface TileBorderNeighbors {
-	[key: string]: number[];
-}
+	private render(): string[][] {
+		const grid = this.tiles.map((x) => x.gridPosition);
+		const xs = grid.map((x) => x?.x || 0);
+		const ys = grid.map((x) => x?.y || 0);
+		const minX = Math.min(...xs);
+		const minY = Math.min(...ys);
+		let result = [];
+		for (let y = 0; y <= Math.max(...ys) - minY; y++) {
+			let imageRow: { [key: number]: string } = {};
+			for (let x = 0; x <= Math.max(...xs) - minX; x++) {
+				grid
+					.find((postition) => postition?.x === x + minX && postition.y === y + minY)
+					?.source.forEach((chars, index) => {
+						imageRow[index] ||= '';
+						imageRow[index] += chars.join('');
+					});
+			}
+			result.push(Object.values(imageRow).join('\n'));
+		}
+		return result
+			.join('\n')
+			.split('\n')
+			.map((x) => x.split(''));
+	}
 
-interface Coordinate {
-	x: number;
-	y: number;
-}
+	private positionNeighbors(index: number): void {
+		const tile = this.tiles[index];
+		if (!tile.gridPosition || !tile.neighborIds?.length) return;
+		const { source, x, y } = tile.gridPosition;
+		const [top, bottom, left, right] = Tile.getBordersFromSource(source);
+		tile.neighborIds
+			.map((id) => this.tiles.findIndex((x) => x.id === id))
+			.forEach((index) => {
+				const neighbor = index >= 0 ? this.tiles[index] : null;
+				if (!neighbor || neighbor.gridPosition) return;
+				for (const nSource of TileImage.possibleOrientationsGenerator(neighbor.source)) {
+					const [nTop, nBottom, nLeft, nRight] = Tile.getBordersFromSource(nSource);
+					if (top === nBottom) neighbor.gridPosition = { source: nSource, x, y: y - 1 };
+					else if (bottom === nTop) neighbor.gridPosition = { source: nSource, x, y: y + 1 };
+					else if (left === nRight) neighbor.gridPosition = { source: nSource, x: x - 1, y };
+					else if (right === nLeft) neighbor.gridPosition = { source: nSource, x: x + 1, y };
+					if (neighbor.gridPosition) break;
+				}
+				this.positionNeighbors(index);
+			});
+	}
 
-enum Direction {
-	Top,
-	Right,
-	Bottom,
-	Left,
+	private static *possibleOrientationsGenerator(source: string[][]) {
+		const orientations: ((x: string[][]) => string[][])[] = [
+			(x) => x,
+			(x) => x.map((row) => [...row].reverse()),
+			(x) => [...x].reverse(),
+		];
+		let curr = source;
+		for (const orientation of orientations) {
+			curr = orientation(source);
+			yield curr;
+			for (const _ of [0, 1, 2, 3]) {
+				const length = curr.length;
+				let rotated: string[][] = Array.from({ length }, () => Array.from({ length }));
+				for (let y = 0; y < curr.length; y++)
+					for (let x = 0; x < curr.length; x++) rotated[y][x] = curr[length - 1 - x][y];
+				curr = rotated;
+				yield curr;
+			}
+		}
+	}
 }
